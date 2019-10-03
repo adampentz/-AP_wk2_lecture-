8c7fb69b8e35e96f6017cebdbf87da804c50438c
Adam <55722970+adampentz@users.noreply.github.com>
2019-10-02 18:57:00 -0700

    10/2/19 followAlong

---
+++ src/wk2_lecture.java
@@ -0,0 +1,17 @@
+
+
+public class wk2_lecture {
+	//members
+	private static Helper helper = new Helper();
+	
+	public static void main(String[] args) {
+		//30n^3 + 15n^2 + 10 = , n = 1
+		helper.printPolyDemo(1);
+		//30n^3 + 15n^2 + 10 = , n = 10
+		helper.printPolyDemo(10);
+		//30n^3 + 15n^2 + 10 = , n = 100
+		helper.printPolyDemo(100);
+
+	}
+
+}
